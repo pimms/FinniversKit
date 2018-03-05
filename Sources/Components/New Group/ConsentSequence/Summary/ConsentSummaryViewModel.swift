584164f756363dcc584f96716238623c9b4a8fac
Sverdrup, Ulrik Utheim <ulrik.utheim.sverdrup@finn.no>
2018-03-05 12:27:47 +0100

    Changes the view model data types to use ToggleSwitchViewModel

--- Sources/Components/New Group/ConsentSequence/Summary/ConsentSummaryViewModel.swift
+++ Sources/Components/New Group/ConsentSequence/Summary/ConsentSummaryViewModel.swift
@@ -5,9 +5,9 @@
 public protocol ConsentSummaryViewModel {
     var title: String { get }
     var introDescription: String { get }
-    var recommendationTitle: String { get }
-    var commercialTitle: String { get }
-    var improveTitle: String { get }
     var description: String { get }
     var doneButtonTitle: String { get }
+    var recommendationModel: ToggleSwitchViewModel { get }
+    var commercialModel: ToggleSwitchViewModel { get }
+    var improveModel: ToggleSwitchViewModel { get }
 }
--- Sources/Components/New Group/ConsentSequence/Summary/Demo/ConsentSummaryViewDefaultData.swift
+++ Sources/Components/New Group/ConsentSequence/Summary/Demo/ConsentSummaryViewDefaultData.swift
@@ -6,12 +6,38 @@ import FinniversKit
 
 public struct ConsentSummaryViewDefaultData: ConsentSummaryViewModel {
     public var title = "Oppsummering"
-    public var introDescription = "Dine valg for personvern"
-    public var recommendationTitle = "Anbefalinger"
-    public var commercialTitle = "Smart reklame"
-    public var improveTitle = "Forbedre FINN"
-    public var description = "Du kan alltid endre på dine samtykker senere under dine innstillinger"
+    public var introDescription = "Dine valg"
+    public var description = "Du kan alltid endre på dine samtykker senere under dine innstillinger."
     public var doneButtonTitle = "Ferdig"
 
+    public var recommendationModel: ToggleSwitchViewModel {
+        struct recommendationModel: ToggleSwitchViewModel {
+            public let headerText = "Anbefalinger"
+            public let onDescriptionText = "Vi gir deg relevante tips på forsiden"
+            public let offDescriptionText = "Relevante tips er slått av"
+            public init() {}
+        }
+        return recommendationModel.init()
+    }
+
+    public var commercialModel: ToggleSwitchViewModel {
+        struct commercialModel: ToggleSwitchViewModel {
+            public let headerText = "Smart reklame"
+            public let onDescriptionText = "Vi leter for deg når du gjør andre ting"
+            public let offDescriptionText = "Du får ikke relevant FINN-innhold når du ikke leter"
+            public init() {}
+        }
+        return commercialModel.init()
+    }
+
+    public var improveModel: ToggleSwitchViewModel {
+        struct improveModel: ToggleSwitchViewModel {
+            public let headerText = "Forbedre FINN"
+            public let onDescriptionText = "Dine data hjelper oss lage bedre tjenester"
+            public let offDescriptionText = "Vi bruker ikke dine data til å lage bedre tjenester"
+            public init() {}
+        }
+        return improveModel.init()
+    }
     public init() {}
 }
