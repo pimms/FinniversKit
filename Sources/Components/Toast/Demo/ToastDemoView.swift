b6d088d19ce52e3b6be3442a23e784b99cb50405
Elvis Nuñez <elvisnunez@me.com>
2018-05-23 12:28:11 +0200

    Toast in TableView

--- Sources/Components/Toast/Demo/ToastDemoView.swift
+++ Sources/Components/Toast/Demo/ToastDemoView.swift
@@ -15,70 +15,42 @@ class ToastClass: NSObject, ToastViewDelegate {
 }
 
 public class ToastDemoView: UIView {
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        return tableView
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         setup()
     }
 
-    public required init?(coder aDecoder: NSCoder) { fatalError() }
-    let delegate = ToastClass()
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-    private func setup() {
-        let successToast = ToastView(style: .success)
-        let imageToast = ToastView(style: .sucesssWithImage)
-        let errorToast = ToastView(style: .error)
-        let successButtonToast = ToastView(style: .successButton)
-        let errorButtonToast = ToastView(style: .errorButton)
+    func setup() {
+        addSubview(tableView)
+        tableView.fillInSuperview()
+        tableView.dataSource = self
+
+        tableView.register(UITableViewCell.self)
+
         let animatedToast = ToastView(style: .success)
-
-        successToast.text = "Success"
-        imageToast.text = "Image success"
-        errorToast.text = "Error"
-        successButtonToast.text = "Action success"
-        successButtonToast.buttonText = "Action"
-        errorButtonToast.text = "Action error"
-        errorButtonToast.buttonText = "Undo"
         animatedToast.text = "Animated success"
-
-        successButtonToast.delegate = delegate
-        errorButtonToast.delegate = delegate
-        animatedToast.delegate = delegate
-
         animatedToast.presentFromBottom(view: self, animateOffset: 0, timeOut: 5)
+    }
+}
 
-        successToast.translatesAutoresizingMaskIntoConstraints = false
-        imageToast.translatesAutoresizingMaskIntoConstraints = false
-        errorToast.translatesAutoresizingMaskIntoConstraints = false
-        successButtonToast.translatesAutoresizingMaskIntoConstraints = false
-        errorButtonToast.translatesAutoresizingMaskIntoConstraints = false
+extension ToastDemoView: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
 
-        addSubview(successToast)
-        addSubview(imageToast)
-        addSubview(errorToast)
-        addSubview(successButtonToast)
-        addSubview(errorButtonToast)
-
-        NSLayoutConstraint.activate([
-            successToast.leadingAnchor.constraint(equalTo: leadingAnchor),
-            successToast.trailingAnchor.constraint(equalTo: trailingAnchor),
-            successToast.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-
-            imageToast.leadingAnchor.constraint(equalTo: leadingAnchor),
-            imageToast.trailingAnchor.constraint(equalTo: trailingAnchor),
-            imageToast.topAnchor.constraint(equalTo: successToast.bottomAnchor, constant: 32),
-
-            errorToast.leadingAnchor.constraint(equalTo: leadingAnchor),
-            errorToast.trailingAnchor.constraint(equalTo: trailingAnchor),
-            errorToast.topAnchor.constraint(equalTo: imageToast.bottomAnchor, constant: 32),
-
-            successButtonToast.leadingAnchor.constraint(equalTo: leadingAnchor),
-            successButtonToast.trailingAnchor.constraint(equalTo: trailingAnchor),
-            successButtonToast.topAnchor.constraint(equalTo: errorToast.bottomAnchor, constant: 32),
-
-            errorButtonToast.leadingAnchor.constraint(equalTo: leadingAnchor),
-            errorButtonToast.trailingAnchor.constraint(equalTo: trailingAnchor),
-            errorButtonToast.topAnchor.constraint(equalTo: successButtonToast.bottomAnchor, constant: 32),
-        ])
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(UITableViewCell.self, for: indexPath)
+        cell.textLabel?.text = "Hi"
+        return cell
     }
 }
